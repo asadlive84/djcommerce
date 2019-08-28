889f1acda9e8613e04ebcc03b43128b06ff00b32
Asaduzzaman Sohel <asadlive.sohel@gmail.com>
2019-08-28 13:13:37 +0600

    fix html issue

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
 from core import views
-
+from django.views.generic.base import TemplateView
 app_name = 'core'
+
+
+class CheckOut(TemplateView):
+    pass
+
+class ProductPage(TemplateView):
+    pass
+
 
 urlpatterns =[
     path('', views.items_list, name='items_list'),
+    path('checkout/', CheckOut.as_view(template_name='checkout-page.html'), name="checkout"),
+    #path'checkout/', CheckOut.as_view(template_name='checkout-page.html'), name="checkout"),
+    path('product/', ProductPage.as_view(template_name='product-page.html'), name="product"),
 ]
