b86ec20c34dcb779b87235cd3258ca9b20b8b5ed
Asaduzzaman Sohel <asadlive.sohel@gmail.com>
2019-08-28 11:06:57 +0600

    start core model

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
+    'core.apps.CoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
